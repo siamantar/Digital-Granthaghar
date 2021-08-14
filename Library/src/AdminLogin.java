c4651a235f136f3fe79b5a56151bca038da21350
Siam Shibly Antar <siam.antar@northsouth.edu>
2021-08-14 22:22:02 +0600

    Source file uploaded
    Contains all the source code of the project

---
+++ Library/src/AdminLogin.java
@@ -0,0 +1,143 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JPasswordField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SwingConstants;
+
+public class AdminLogin extends JFrame {
+	static AdminLogin frame;
+	private JPanel contentPane;
+	private JTextField textField;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					frame = new AdminLogin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AdminLogin() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.LIGHT_GRAY);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JLabel lblAdminLoginForm = new JLabel("Admin Login Panel");
+		lblAdminLoginForm.setHorizontalAlignment(SwingConstants.CENTER);
+		lblAdminLoginForm.setForeground(Color.DARK_GRAY);
+		lblAdminLoginForm.setFont(new Font("Myanmar Text", Font.BOLD, 18));
+		
+		JLabel lblEnterName = new JLabel("Enter Name:");
+		lblEnterName.setForeground(Color.DARK_GRAY);
+		lblEnterName.setFont(new Font("Tahoma", Font.BOLD, 13));
+		
+		JLabel lblEnterPassword = new JLabel("Enter Password:");
+		lblEnterPassword.setForeground(Color.DARK_GRAY);
+		lblEnterPassword.setFont(new Font("Tahoma", Font.BOLD, 13));
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			String name=textField.getText();
+			String password=String.valueOf(passwordField.getPassword());
+			if(name.equals("admin")&&password.equals("admin123")){
+				AdminSuccess.main(new String[]{});
+				frame.dispose();
+			}else{
+				JOptionPane.showMessageDialog(AdminLogin.this, "Sorry, Username or Password Error","Login Error!", JOptionPane.ERROR_MESSAGE);
+				textField.setText("");
+				passwordField.setText("");
+			}
+			}
+		});
+		
+		passwordField = new JPasswordField();
+		
+		JButton btnLogin_1 = new JButton("Back");
+		btnLogin_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Library.main(new String[]{});
+				frame.dispose();
+			}
+		});
+		btnLogin_1.setFont(new Font("Tahoma", Font.BOLD, 13));
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(btnLogin_1)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(btnLogin, GroupLayout.PREFERRED_SIZE, 71, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(19)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblEnterName)
+								.addComponent(lblEnterPassword))
+							.addGap(47)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+								.addComponent(passwordField)
+								.addComponent(textField, GroupLayout.DEFAULT_SIZE, 185, Short.MAX_VALUE))))
+					.addContainerGap(66, Short.MAX_VALUE))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblAdminLoginForm, GroupLayout.DEFAULT_SIZE, 424, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(8)
+					.addComponent(lblAdminLoginForm)
+					.addGap(32)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblEnterName)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(28)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblEnterPassword)
+						.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnLogin)
+						.addComponent(btnLogin_1))
+					.addContainerGap(60, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+}
