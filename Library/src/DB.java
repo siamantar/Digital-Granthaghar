3083a418654ae48176ee431201c7f05918761a81
Siam Shibly Antar <siam.antar@northsouth.edu>
2021-08-26 19:59:24 +0600

    Connects the Application program to the Database

---
+++ Library/src/DB.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DB {
+	public static Connection getConnection(){
+		Connection con=null;
+		try{
+			//Class.forName("com.mysql.jdbc.Driver");
+			//con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","","");
+			
+			DriverManager.registerDriver(new org.sqlite.JDBC());
+            String url = "jdbc:sqlite:lms.db";
+            con = DriverManager.getConnection(url);
+            
+		}catch(Exception e){
+			System.out.println(e);
+		}
+		return con;
+	}
+
+}
